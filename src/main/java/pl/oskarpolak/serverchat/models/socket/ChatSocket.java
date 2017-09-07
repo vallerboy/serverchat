780fc80585245dd0f28f30fa7c44933e51c1c1cb
Oskar <oskarbank@gmail.com>
2017-09-07 13:10:23 +0200

    ad

--- src/main/java/pl/oskarpolak/serverchat/models/socket/ChatSocket.java
+++ src/main/java/pl/oskarpolak/serverchat/models/socket/ChatSocket.java
@@ -28,12 +28,26 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        session.sendMessage(new TextMessage("Hejo!"));
+        session.sendMessage(new TextMessage("Twoja pierwsza wiadomość zostanie Twoim nickiem!"));
         userList.add(new UserModel(session));
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 
+        UserModel userModel = userList.stream()
+                .filter(s -> s.getSession().equals(session))
+                .findAny()
+                .get();
+
+        if(userModel.getNick() == null){
+            userModel.setNick(message.getPayload());
+            userModel.getSession().sendMessage(
+                    new TextMessage("Ustawiliśmy Twój nick!")
+            );
+            return;
+        }
 
         userList.forEach(s -> {
                     try {
